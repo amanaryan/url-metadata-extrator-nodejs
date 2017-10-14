b1057df6f13aba8ea63ea27538f92c8c8d8abb0a
AMAN ARYAN <mail@amanaryan.com>
2017-10-14 17:50:13 +0530

    fix for case where multiple meta tags for same cause are present

--- api.js
+++ api.js
@@ -14,17 +14,17 @@ var url = req.query.url ;
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
 
-      var ex_title = $('meta[property="og:title"]')[0]['attribs']['content'];
-      var ex_url = $('meta[property="og:url"]')[0]['attribs']['content'];
-      var ex_description = $('meta[property="og:description"]')[0]['attribs']['content'];
-      var ex_image = $('meta[property="og:image"]')[0]['attribs']['content'];
+      var ex_title = $('meta[property="og:title"]');
+      var ex_url = $('meta[property="og:url"]');
+      var ex_description = $('meta[property="og:description"]');
+      var ex_image = $('meta[property="og:image"]');
 
       res_json = {
         original_url:url,
-        url:ex_url,
-        title:ex_title,
-        description:ex_description,
-        thumbnail_url:ex_image
+        url:ex_url[ex_url.length-1]['attribs']['content'],
+        title:ex_title[ex_title.length-1]['attribs']['content'],
+        description:ex_description[ex_description.length-1]['attribs']['content'],
+        thumbnail_url:ex_image[ex_image.length-1]['attribs']['content']
       }
 
       res.json(res_json);
